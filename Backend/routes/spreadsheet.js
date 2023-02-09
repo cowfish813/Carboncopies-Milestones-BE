0aac266891f5ba91918e9a7bd6bf462470e81b6f
Nicholas Cheung <ncheung@carboncopies.org>
2023-02-08 16:33:03 -0800

    post to drive working

--- Backend/routes/spreadsheet.js
+++ Backend/routes/spreadsheet.js
@@ -4,48 +4,60 @@ const neo4j = require('neo4j-driver');
 const { v4: uuidv4 } = require('uuid');
 const { NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD } = process.env;
 const driver = new neo4j.driver(NEO4J_URI, neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD));
-
-const Milestone = require('../models/milestone');
 const axios = require('axios');
 
+// const Milestone = require('../models/milestone');
 const fs = require('fs');
 const sheetName = `${Date.now()}ccf${uuidv4()}.json`;
+// const sheetName = `${Date.now()}ccf${uuidv4()}.csv`;
 const path = require('path')
+
+
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
-// const process = require('process');
+const proc = require('process');
+const {GoogleAuth, OAuth2Client} = require('google-auth-library');
+const { application } = require('express');
+const {CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN} = process.env;
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
-const {GoogleAuth} = require('google-auth-library');
 
-async function uploadBasic(fileToDrive) {
-  // Get credentials and build service
-  // TODO (developer) - Use appropriate auth mechanism for your app
-    const auth = new GoogleAuth({
-        scopes: 'https://www.googleapis.com/auth/drive',
-    });
-    const service = google.drive({version: 'v3', auth});
-    const fileMetadata = {
-        name: sheetName,
-    };
+// const TOKEN_PATH = path.join(proc.cwd(), 'token.json');
+// const CREDENTIALS_PATH = path.join(proc.cwd(), 'credentials.json');
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
-    const media = {
-        mimeType: 'application/json',
-        body: fs.createReadStream(fileToDrive),
-    };
+// const filePath = path.join(__dirname, '');
+const drive = google.drive({
+    version: 'v3',
+    auth: oAuth2Client
+})
+
+const postToDrive = async (name, file) => {
+    const config =  {
+        headers: {
+            "Content-Type": 'application/json',
+        }
+    }
+    const url = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media';
 
     try {
-        const file = await service.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id',
-        });
-        console.log('File Id:', file.data.id);
-        return file.data.id;
-    } catch (err) {
-        console.log(err);
-        throw err;
+        const response = await drive.files.create({
+            requestBody: {
+                name: name,
+                mimeType: 'application/json', //mimetype package can identify file
+            },
+            media: {
+                mimeType: 'aplpication/json',
+                body: JSON.stringify(file) //fs.createReadStream() //provide filepath. whats an alternative?
+            }
+        })
+        console.log(response.data);
+    } catch (e) {
+        console.log(e.message);
     }
 }
+
 
 router.get('/', async (req, res) => {
     const cypher = `
@@ -57,15 +69,16 @@ router.get('/', async (req, res) => {
     try {
         const result = await session.run(cypher);
         const data = result.records.map(record => record._fields[4]);
-        // const file = fs.writeFileSync(sheetName, data[0]);
-
-        // uploadBasic(file);
+        postToDrive(sheetName, data);
         res.send(data);
         session.close();
     } catch (e) {
-        console.log(e);
+        console.log(e, 'failed GET sheets');
     }
 })
+
+
+
 
 // file:///1674858879743ccf6f82d3f2-00f4-492c-9f0f-9aa1e9249f83.csv
 //test link:  https://docs.google.com/spreadsheets/d/1jGbsFhfOIl375_EaGSjb8Ur9sbhvO9UURqdDJ6QoRGc/edit?usp=sharing
