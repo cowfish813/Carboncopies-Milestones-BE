d6e195f6f35cd98ac6fed17aed10fb782240133f
Nicholas Cheung <ncheung@carboncopies.org>
2023-03-10 13:31:12 -0800

    updates

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,13 @@ dotenv.config();
 const express = require('express');
 const milestones = require('./routes/milestones.js');
 const spreadsheet = require('./routes/spreadsheet.js');
-
 const app = express();
+const cors = require('cors');
+app.use(cors(3000));
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(express.json());
 app.use('/api/milestones', milestones);
 app.use('/api/spreadsheet', spreadsheet);
+
