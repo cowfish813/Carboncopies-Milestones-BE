e8c5ea057983be64e5e02fcad5af1d8eebca7fc8
Nicholas Cheung <ncheung@carboncopies.org>
2023-03-24 12:13:41 -0700

    test route

--- Backend/routes/spreadsheet.js
+++ Backend/routes/spreadsheet.js
@@ -4,9 +4,6 @@ const neo4j = require('neo4j-driver');
 const { v4: uuidv4 } = require('uuid');
 const {google} = require('googleapis');
 
-const fs = require('fs');
-const { Parser } = require('@json2csv/plainjs');
-
 const { 
     NEO4J_URI, 
     NEO4J_USERNAME, 
@@ -28,42 +25,43 @@ const oAuth2Client = new google.auth.OAuth2(
     REDIRECT_URI,
 );
 
-const googleSharedDriveID = '1Spm0zSrUPb4McJjvNylngzWHmJKF8VXy';//Shared Drives - Education - Roadmap Vis
+const googleSharedDriveID = '1Spm0zSrUPb4McJjvNylngzWHmJKF8VXy';
+    //Shared Drives - Education - Roadmap Vis
 
 oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
-const postToDrive = async (name, file) => {
-    const googleAuth = { version: 'v3', auth: oAuth2Client };
-    const drive = google.drive(googleAuth);
+// const postToDrive = async (name, file) => {
+//     const googleAuth = { version: 'v3', auth: oAuth2Client };
+//     const drive = google.drive(googleAuth);
 
-    const fileMetaData = {
-        name,
-        mimeType: 'text/json',
-    };
-    const media = {
-        mimeType: 'text/json',
-        body: JSON.stringify(file)
-    };
-    const requestBody = {
-        name,
-        resource: fileMetaData,
-        mimeType: 'text/json', //mimetype package can identify file
-        parents: [googleSharedDriveID]
-    };
+//     const fileMetaData = {
+//         name,
+//         mimeType: 'text/json',
+//     };
+//     const media = {
+//         mimeType: 'text/json',
+//         body: JSON.stringify(file)
+//     };
+//     const requestBody = {
+//         name,
+//         resource: fileMetaData,
+//         mimeType: 'text/json', //mimetype package can identify file
+//         parents: [googleSharedDriveID]
+//     };
 
-    try {
-        const response = await drive.files.create({
-            supportsAllDrives: true,
-            requestBody,
-            media,
-            fields: 'id, name'
-        })
+//     try {
+//         const response = await drive.files.create({
+//             supportsAllDrives: true,
+//             requestBody,
+//             media,
+//             fields: 'id, name'
+//         })
 
-        console.log('drive ID:', response.data.id);
-    } catch (e) {
-        console.log(e.message, e);
-    }
-};
+//         console.log('drive ID:', response.data.id);
+//     } catch (e) {
+//         console.log(e.message, e);
+//     }
+// };
 
 const downloadFromDrive = async (drive, fileId) => {1
     const res = await drive.files.get({
@@ -88,7 +86,6 @@ router.post('/', async (req, res) => {
         RETURN row
     `
 
-
     const session = driver.session();
     const updated = new Date(Date.now()).toString();
     const url = req.body.url;
@@ -103,30 +100,30 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
-    //downloads from neo4j
-    //uploads to google drive
-    const cypher = `
-        CALL apoc.export.json.all(null, {stream:true})
-        YIELD file, nodes, relationships, properties, data
-        RETURN file, nodes, relationships, properties, data
-        `;
-    const session = driver.session();
-    try {
-        // const parser = new Parser();
-        
-        const result = await session.run(cypher);
-        const data = result.records.map(record => record._fields[4]);
 
-        // const csv = parser.parse(result)
-        // postToDrive(csvName, data);
-        postToDrive(jsonName, result);
-        res.send(data);
-        session.close();
-    } catch (e) {
-        console.log(e);
-    }
-});
+//would be JSON but might not be neccessary anymore
+// router.get('/', async (req, res) => { 
+//     //downloads from neo4j
+//     //uploads to google drive
+//     const cypher = `
+//         CALL apoc.export.json.all(null, {stream:true})
+//         YIELD file, nodes, relationships, properties, data
+//         RETURN file, nodes, relationships, properties, data
+//         `;
+//     const session = driver.session();
+//     try {
+//         // const parser = new Parser();
+//         const result = await session.run(cypher);
+//         const data = result.records.map(record => record._fields[4]);
+//         // const csv = parser.parse(result)
+//         // postToDrive(csvName, data);
+//         postToDrive(jsonName, result);
+//         res.send(data);
+//         session.close();
+//     } catch (e) {
+//         console.log(e);
+//     }
+// });
 
 router.get('/csv', async (req, res) => {
     const cypher = ` 
