e1aae9dcf3a975864820cb0c00b21f566dc6a706
Nicholas Cheung <ncheung@carboncopies.org>
2023-10-17 16:42:57 -0700

    user model schema

---
+++ models/user.js
@@ -0,0 +1,14 @@
+this.const = { v4: uuidv4 } = require('uuid');
+
+class User {
+    constructor(val) {
+        this.user_id = uuidv4();
+        this.email = val.email;
+        this.password = val.password;
+        this.property = val.property;
+        this.created_at = new Date(Date.now()).toString();
+        this.updated_at = new Date(Date.now()).toString();
+    }
+}
+
+module.exports = User;
